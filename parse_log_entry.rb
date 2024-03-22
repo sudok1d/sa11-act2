dd02861197c8d1338d077f2f20d4124f1d120567
Asiah Clark <clarkaa0@gmail.com>
2024-03-21 19:54:07 -0500

    Added solution to problem 5

---
+++ parse_log_entry.rb
@@ -0,0 +1,7 @@
+def parse_log_entry(log)
+  pattern = /\A(INFO|WARN|ERROR): (.+)\z/
+  match = log.match(pattern)
+  [match[1], match[2]] if match
+  end
+log_entry = "ERROR: Disk space low"
+puts parse_log_entry(log_entry).inspect
